3081fb0dc700ff7d267f605ce64b24310c056ae1
Akshay Sharma <itsruaks@gmail.com>
2021-11-23 21:36:48 +0530

    and file completed

--- Utils/and.py
+++ Utils/and.py
@@ -1,6 +1,7 @@
-from Utils.model import Perceptron
-from Utils.all_utils import prepare_data
-
+from model import Perceptron
+from all_utils import prepare_data, save_model, save_plot
+import pandas as pd
+import numpy as np
 #AND gate data
 
 data={"x1":[0,0,1,1],"x2":[0,1,0,1],"y":[0,0,0,1]}
@@ -14,3 +15,6 @@ EPOCHS= 10
 model= Perceptron(eta=ETA,epochs=EPOCHS)
 model.fit(x,y)
 _=model.total_loss()
+
+save_model(model, filename="and.model")
+save_plot(AND,"and.png",model)
